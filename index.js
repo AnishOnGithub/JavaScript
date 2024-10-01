2d83665451151c06a8db74c5c292cb979fcf8964
AnishOnGithub <anish.taklikar07@gmail.com>
2024-10-01 19:10:29 +0530

    AvgMarksUsingArrays

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log("hello");
-console.log("hello");
+// Arrays
 
+//code to print average marks of the class
+let marks=[92,84,75,40];
+console.log("The marks of the students are as follows:" ,marks)
+
+
+let sum=0;
+for(let i=0; i< marks.length; i++){
+    sum=sum+marks[i];
+}
+let average= sum/marks.length;
+console.log(average);
