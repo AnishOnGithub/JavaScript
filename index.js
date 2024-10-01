3d34a196e6ef179a6f488f92d13fa28a53a5d3fa
AnishOnGithub <anish.taklikar07@gmail.com>
2024-10-01 21:19:02 +0530

    QuestionOnArraysAndLoops

--- index.js
+++ index.js
@@ -28,3 +28,43 @@ let marks=[92,84,75,40];
     
 //     console.log(country);
 // }
+
+//average of class marks usinf for loop
+
+// marksOfStudnets=[85,97,44,37,76,60];
+// let sum =0;
+// for(i=0;i<marksOfStudnets.length;i++){
+//   sum=sum+marksOfStudnets[0];
+// }
+// console.log("the total marks of the class:",sum);
+// let average=sum/marksOfStudnets.length
+
+// console.log("the average of the class is: ",average)
+
+//average marks of the class using for off loop
+
+// marksOfStudnets=[85,97,44,37,76,60];
+// let sum=0;
+// for(let mark of marksOfStudnets){
+//    sum=sum+mark;
+   
+// }
+// console.log(sum);
+// let average=sum/marksOfStudnets.length;
+// console.log("The average of the class is ",average);
+
+//A shop has a discount of 10% on each item you buy, calculate the discounted price
+
+let prices=[250,645,300,900,50]
+let discount,discountedPrice, i=0
+
+
+for( let price of prices){
+    i++
+    discount=price/10;
+    discountedPrice=price-discount
+    
+    console.log(`The price of item ${i} is ${price}rs and the discounted price is ${discountedPrice}rs for a discount of ${discount}rs`)
+    
+}
+
