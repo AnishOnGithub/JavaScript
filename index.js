c5ce68796c5b0fe700221652a208d98e7124d333
AnishOnGithub <anish.taklikar07@gmail.com>
2024-10-01 19:40:41 +0530

    ForOfLoop

--- index.js
+++ index.js
@@ -2,12 +2,29 @@
 
 //code to print average marks of the class
 let marks=[92,84,75,40];
-console.log("The marks of the students are as follows:" ,marks)
+// console.log("The marks of the students are as follows:" ,marks)
 
 
-let sum=0;
-for(let i=0; i< marks.length; i++){
-    sum=sum+marks[i];
-}
-let average= sum/marks.length;
-console.log(average);
+// let sum=0;
+// for(let i=0; i<marks.length; i++){
+//     sum=sum+marks[i];
+// }
+// let average= sum/marks.length;
+// console.log(average);
+
+
+//using for of loop
+
+// let sum=0
+
+// for (let mark of marks){
+//  sum+=mark;
+// }
+// console.log(sum);
+
+// let countries=["india","norway","us","uk"];
+// console.log("The countries are: ")
+// for (let country of countries){
+    
+//     console.log(country);
+// }
