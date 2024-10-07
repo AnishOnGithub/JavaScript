068b03f1891cf9e01d825d8daf06b7baa100e9b0
AnishOnGithub <anish.taklikar07@gmail.com>
2024-10-07 20:39:00 +0530

    MethodsOfArrays

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Arrays
 
 //code to print average marks of the class
-let marks=[92,84,75,40];
+// let marks=[92,84,75,40];
 // console.log("The marks of the students are as follows:" ,marks)
 
 
@@ -55,16 +55,99 @@ let marks=[92,84,75,40];
 
 //A shop has a discount of 10% on each item you buy, calculate the discounted price
 
-let prices=[250,645,300,900,50]
-let discount,discountedPrice, i=0
+// let prices=[250,645,300,900,50]
+// let discount,discountedPrice, i=0
 
 
-for( let price of prices){
-    i++
-    discount=price/10;
-    discountedPrice=price-discount
+// for( let price of prices){
+//     i++
+//     discount=price/10;
+//     discountedPrice=price-discount
     
-    console.log(`The price of item ${i} is ${price}rs and the discounted price is ${discountedPrice}rs for a discount of ${discount}rs`)
+//     console.log(`The price of item ${i} is ${price}rs and the discounted price is ${discountedPrice}rs for a discount of ${discount}rs`)
     
-}
+// }
 
+
+
+//methods of arrays 
+
+//push --> adds items to the end of the arrays without creating a new array 
+
+// let foodItems= [ "chips, pizza, burger, paneer"]
+
+// foodItems.push("kurkure, spices")
+
+// console.log(foodItems)
+
+//pop --> delete item from end and return it, in the same array
+
+// let foodItems= [ "chips", "pizza", "burger", "paneer"];
+// console.log(foodItems);
+// let deletedItems= foodItems.pop();
+// console.log(foodItems);
+// console.log("deleted item is: ",deletedItems)
+
+//toString --> converts arrays to strings 
+
+// let foodItems= [ "chips", "pizza", "burger", "paneer"];
+
+// console.log(foodItems.toString())
+
+// let percentage= [98,75,60,52,41];
+// console.log(percentage);
+
+// console.log("Converting to string type=",percentage.toString());
+
+
+
+//concat --> joins multiple arrays and returns a new one 
+
+
+//  let fruit=["apple", "banana", "kiwi"];
+//  let veggies=["potato","onion","beetroot"];
+//  let dryFruits=["almonds","pista","walnut"]
+//  let cart=fruit.concat(veggies,dryFruits)
+//  console.log("Fruits and veggies are: ",cart)
+
+
+
+//unshift --> add to start 
+//shift --> works like pop --> adds to the end 
+
+// fruit.unshift("mango");
+// console.log(fruit)
+// cart= fruit.unshift("mango");
+// console.log(fruit)
+
+
+//slice--> gives a piece of array or string without changing the main array 
+
+
+// let fruit=["apple", "banana", "kiwi","mango","guava","lichi","pineapple"];
+
+// console.log(fruit.slice(0,1)) //dosent include the last index
+
+//splice--> this helps in deleting,replacing and adding an array, adding a new item in the array
+
+// let editedList=fruit.splice(1,2,"coconut")
+
+// console.log(fruit)
+
+
+
+
+//Q)Create an array to store companies "bloomberg"," Microsoft","Uber","Ibm","Netflix"
+
+//code to delete the 1st company
+// companies= ["Bloomberg"," Microsoft","Uber","Ibm","Netflix"]
+// companies.splice(0,1)
+// console.log(companies)
+
+//code to remove uber and ola 
+// companies.splice(2,1,"Ola")
+// console.log(companies)
+
+// to add amazon at the end 
+// companies.push("Amazon")
+// console.log(companies)
